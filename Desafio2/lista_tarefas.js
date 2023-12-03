e6f7e67122d0b5a4bd856c342364e7db88c56cdc
Samuel Nunes <samuelnunes4039@gmail.com>
2023-12-02 21:40:58 -0300

    Lista de tarefas do desafio 2

--- Desafio2/lista_tarefas.js
+++ Desafio2/lista_tarefas.js
@@ -1,17 +1,16 @@
-
-function nova_tarefa(nome = "indefinido", status = "nao_iniciado") {
+function novaTarefa(nome = "indefinido", status = "nao_iniciado") {
     this.nome = nome
     this.status = status
 }
-tarefa1 = new nova_tarefa("Responder bea no discord", "Não concluída"),
-tarefa2 = new nova_tarefa("Sofrer pelo Santa Cruz", "Concluída"),
-tarefa3 = new nova_tarefa("Mudar algo no boilerplate", "Não concluída")
+tarefa1 = new novaTarefa("Responder bea no discord", "Não concluída"),
+tarefa2 = new novaTarefa("Sofrer pelo Santa Cruz", "Concluída"),
+tarefa3 = new novaTarefa("Mudar algo no boilerplate", "Não concluída")
 
-let lista_tarefas = [tarefa1, tarefa2, tarefa3]
-lista_tarefas.push(tarefa4 = new nova_tarefa("Colocar o vídeo certo no treinamento de JS kkkkk <3", "Concluída")) // Apenas uma descontração kkk
+let listaTarefas = [tarefa1, tarefa2, tarefa3]
+listaTarefas.push(tarefa4 = new novaTarefa("Colocar o vídeo certo no treinamento de JS kkkkk <3", "Concluída")) // Apenas uma descontração kkk
 
-for(let i = 0; i < lista_tarefas.length; i = i + 1){
+for(let i = 0; i < listaTarefas.length; i = i + 1){
     console.log(`Tarefa ${i + 1}:
-        Nome: ${lista_tarefas[i].nome}
-        Status: ${lista_tarefas[i].status}`)
+        Nome: ${listaTarefas[i].nome}
+        Status: ${listaTarefas[i].status}`)
 }
